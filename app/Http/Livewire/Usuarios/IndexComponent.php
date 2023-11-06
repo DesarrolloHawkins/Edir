d7dfedf6568742968fe14e332eaf3c3c8d568c8b
antoniosanchezwebdev <128600888+antoniosanchezwebdev@users.noreply.github.com>
2023-11-06 16:05:23 +0100

    Initial commit

---
+++ app/Http/Livewire/Usuarios/IndexComponent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Livewire\Usuarios;
+
+use App\Models\User;
+use Livewire\Component;
+
+class IndexComponent extends Component
+{
+    // public $search;
+    public $usuarios;
+
+    public function mount()
+    {
+        $this->usuarios = User::all();
+    }
+
+    public function render()
+    {
+
+        return view('livewire.usuarios.index-component');
+    }
+
+    public function getRole($id)
+    {
+        switch ($id) {
+            case 1:
+                return 'Administrador';
+                break;
+            case 2:
+                return 'Secretaría';
+                break;
+            case 3:
+                return 'Comodoro';
+                break;
+            case 4:
+                return 'Marinero';
+                break;
+            case 5:
+                return 'Acceso info';
+                break;
+            default:
+                return '';
+                break;
+        }
+    }
+}
