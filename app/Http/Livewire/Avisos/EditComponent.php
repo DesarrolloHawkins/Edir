adcb009e94ff5e8f9f0829e2b14a9ef3e7c6570f
crmhawkins <128600888+crmhawkins@users.noreply.github.com>
2024-08-29 14:03:49 +0200

    correccion en edit de anuncion

--- app/Http/Livewire/Avisos/EditComponent.php
+++ app/Http/Livewire/Avisos/EditComponent.php
@@ -126,8 +126,7 @@ class EditComponent extends Component
         foreach ($this->anuncio->users as $user) {
             // Aquí accedemos al campo 'status' de la tabla pivote.
             $status = $user->pivot->status;
-            $comunidad = Comunidad::where('id', $user->comunidad_id)->first() != null ? Comunidad::where('
-            ', $user->id)->first()->nombre : "Comunidad no asignada";
+            $comunidad = Comunidad::where('id', $user->comunidad_id)->first() != null ? Comunidad::where('id', $user->comunidad_id)->first()->nombre : "Comunidad no asignada";
             // Basado en el estado, añadimos el string correspondiente al array.
             $usuariosEstatus[] = "<b>" . $user->name . " (" . $comunidad . ")</b>: " . ($status == 0 ? "No leído" : "Leído");
         }
